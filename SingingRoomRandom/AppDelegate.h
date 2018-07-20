fee1a43b45d20f36e3d024518ee90fe7e4cda6ad
박지훈 <jihoonpark@jihoonparkui-macbook-pro.local>
2018-07-20 17:34:18 +0900

    Initial Commit

---
+++ SingingRoomRandom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SingingRoomRandom
+//
+//  Created by JihoonPark on 2018. 7. 20..
+//  Copyright © 2018년 JihoonPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
